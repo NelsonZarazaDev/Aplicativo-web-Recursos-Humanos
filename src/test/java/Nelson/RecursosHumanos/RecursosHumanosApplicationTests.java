827ad49bf86efa8a3b7ed44bbbd068eefc6e5637
Nelson Mauricio <nelsonmauricionavarrozaraza@gmail.com>
2024-03-14 16:10:23 -0500

    first commit

---
+++ src/test/java/Nelson/RecursosHumanos/RecursosHumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package Nelson.RecursosHumanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursosHumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
